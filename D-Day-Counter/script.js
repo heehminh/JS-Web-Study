b1be4155efef50f6006a5cc2023eea6ac6f1cf65
heehminh <mink141416@gmail.com>
2023-02-12 18:33:05 +0900

    [FEAT] localStorage에 date 저장

--- D-Day-Counter/script.js
+++ D-Day-Counter/script.js
@@ -1,9 +1,8 @@
 const messageContainer = document.querySelector("#d-day-message");
 const container = document.querySelector("#d-day-container");
-const intervalIdArr = [];
+const savedDate = localStorage.getItem("saved-date");
 
-container.style.display = "none";
-messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
+const intervalIdArr = [];
 
 const dateFormMaker = () => {
   const inputYear = document.querySelector("#target-year-input").value;
@@ -16,6 +15,10 @@ const dateFormMaker = () => {
 };
 
 const counterMaker = (targetDateInput) => {
+  if (targetDateInput !== savedDate) {
+    // 받아오는 데이터와 저장된 데이터가 다를 때
+    localStorage.setItem("saved-date", targetDateInput);
+  }
   const nowDate = new Date();
   const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0); // 자정을 기준으로 세팅
   const remaining = (targetDate - nowDate) / 1000; // remaining: 초 기준으로 작성됨
@@ -57,8 +60,12 @@ const counterMaker = (targetDateInput) => {
   }
 };
 
-const starter = () => {
-  const targetDateInput = dateFormMaker();
+const starter = (targetDateInput) => {
+  console.log(targetDateInput);
+  if (!targetDateInput) {
+    targetDateInput = dateFormMaker();
+  }
+
   container.style.display = "flex";
   messageContainer.style.display = "none";
   setClearInterval(); // 버튼이 눌리면 기존 interval 모두 삭제
@@ -77,8 +84,16 @@ const setClearInterval = () => {
 };
 
 const resetTimer = () => {
+  localStorage.removeItem("saved-date");
   container.style.display = "none";
   messageContainer.style.display = "flex";
   messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
   setClearInterval();
 };
+
+if (savedDate) {
+  starter(savedDate);
+} else {
+  container.style.display = "none";
+  messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
+}
