cfecd54ce7c5175bec6131c1c27d18006f01d950
heehminh <mink141416@gmail.com>
2023-02-12 21:58:26 +0900

    [FEAT] 완료한 일 체크

--- ToDo-List/script.js
+++ ToDo-List/script.js
@@ -1,15 +1,25 @@
 const todoInput = document.querySelector("#todo-input");
 
+const createTodo = () => {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  console.log(newLi);
+  todoInput.value = "";
+};
+
 const keyCodeCheck = () => {
   if (window.event.keyCode === 13 && todoInput.value) {
-    const todoList = document.querySelector("#todo-list");
-
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-
-    newSpan.textContent = todoInput.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = "";
+    createTodo();
   }
 };
