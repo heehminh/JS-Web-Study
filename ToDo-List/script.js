cccead6fa0756036facf0eaa0dfa969c72f858b4
heehminh <mink141416@gmail.com>
2023-02-13 16:16:50 +0900

    [FEAT] Todo 객체 local storage에 저장

--- ToDo-List/script.js
+++ ToDo-List/script.js
@@ -1,25 +1,75 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const savedToDoList = JSON.parse(localStorage.getItem("saved-items"));
 
-const createTodo = () => {
-  const todoList = document.querySelector("#todo-list");
+const saveItemsFn = () => {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+  // local storage는 문자열로만 저장이 가능한데
+  // 배열이나 객체는 문자열로 저장할 수 없으므로
+  // JSON 형식으로 변환시켜준다.
+};
+
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
 
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
 
-  newSpan.textContent = todoInput.value;
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  if (storageData?.complete) {
+    // storageData && storageData.complete 와 동일
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
-  console.log(newLi);
   todoInput.value = "";
+
+  saveItemsFn();
 };
+
+if (savedToDoList) {
+  for (let i = 0; i < savedToDoList.length; i++) {
+    createTodo(savedToDoList[i]);
+  }
+}
 
 const keyCodeCheck = () => {
   if (window.event.keyCode === 13 && todoInput.value) {
     createTodo();
   }
+};
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
+  }
+  saveItemsFn();
 };
