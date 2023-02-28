55ae69d57a18ea07b556f70a5c817f5f6b78baf5
heehminh <mink141416@gmail.com>
2023-02-28 21:50:21 +0900

    Study: JSON to Object

--- JSON/json.js
+++ JSON/json.js
@@ -1,6 +1,7 @@
 // JSON: Javascript Object Notation
 
 // 1. Object to JSON
+// stringfy(obj)
 let json = JSON.stringify(true);
 
 // Array -> JSON
@@ -41,3 +42,23 @@ json = JSON.stringify(rabbit, (key, value) => {
 // json.js:32 key: jump, value: () => {
 //     console.log(`${name} can jump`);
 //   }
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json);
+console.log(obj);
+
+rabbit.jump(); // can jump // rabbit object안에는 jump 메서드가 있음
+// obj.jump(); // (Error) Uncaught TypeError: obj.jump is not a function
+// note! obj -> JSON -> obj 에선 원래 obj의 메서드를 사용할 수 없다
+
+console.log(rabbit.birthDate.getDate()); // birthDate: new Date()의 내장 메서드를 사용할 수 있음
+console.log(obj.birthDate); // birthDate가 단순한 문자열임
+
+const obj2 = JSON.parse(json, (key, value) => {
+  return key === "birthDate" ? new Date(value) : value;
+});
+console.log(rabbit.birthDate.getDate());
+console.log(obj2.birthDate.getDate()); // new Date() 안의 내장 메서드를 사용할 수 있음
